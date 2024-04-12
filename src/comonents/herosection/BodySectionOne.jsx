ad570790220338cbafec0beb361e0f5efaab942f
Amar pratap <amarprataphzb@gmail.com>
2024-04-12 13:53:34 +0530

    bodysectionone changes

--- src/comonents/herosection/BodySectionOne.jsx
+++ src/comonents/herosection/BodySectionOne.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import image from '../../assets/body1background.png'
 
 const BodySectionOne = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div  className='w-screen h-dvh' style={{backgroundImage:`url(${image})`,backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
+    <h1 className='text-7xl text-white font-bold' >Elevate Your Employee Wellness with Restory Healthytech's AI-Driven Health Check!</h1>
+      <h4 className='text-white font-bold'>Give your employees the most advanced and AI driven  annual health checkup</h4>
+      <button className=' w-max text-white rounded-md'>Learn More</button>
+    
+    </div>
   )
 }
 

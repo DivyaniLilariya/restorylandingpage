b152f7fbc0dee6d17bb8ab28e99097b753822662
Amar pratap <amarprataphzb@gmail.com>
2024-04-12 12:33:17 +0530

    tailwind changes

--- src/comonents/herosection/BodySectionOne.jsx
+++ src/comonents/herosection/BodySectionOne.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const BodySectionOne = () => {
   return (
-    <div><h2>changes in bodyyyy</h2></div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 

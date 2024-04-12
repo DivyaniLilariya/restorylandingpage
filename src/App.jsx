ea82242e8117dd05fa84ee94bb113035a31f2eba
Amar pratap <amarprataphzb@gmail.com>
2024-04-12 12:06:09 +0530

    kuch nahi

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import './App.css'
-
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-    helo amar
-  </h1>
+      <h1 className="text-3xl font-bold underline">Hello world! helo amar</h1>
 
-     <h2>helloo divyaniiiiii how r u</h2>
+      <h2>helloo divyaniiiiii how r u</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

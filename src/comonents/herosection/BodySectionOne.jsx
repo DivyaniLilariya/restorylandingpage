4c500b81e89ebb4cdeda237fbc6ba2134c12c7b4
Amar pratap <amarprataphzb@gmail.com>
2024-04-12 15:04:20 +0530

    bodysection 1 page completed

--- src/comonents/herosection/BodySectionOne.jsx
+++ src/comonents/herosection/BodySectionOne.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import image from '../../assets/body1background.png'
+import React from "react";
+import image from "../../assets/body1background.png";
 
 const BodySectionOne = () => {
   return (
-    <div  className='w-screen h-dvh' style={{backgroundImage:`url(${image})`,backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
-    <h1 className='text-7xl text-white font-bold' >Elevate Your Employee Wellness with Restory Healthytech's AI-Driven Health Check!</h1>
-      <h4 className='text-white font-bold'>Give your employees the most advanced and AI driven  annual health checkup</h4>
-      <button className=' w-max text-white rounded-md'>Learn More</button>
-    
+    <div
+      className=" h-dvh"
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      
+      <h1 className="text-6xl pt-36 text-center  text-white font-bold">
+        Elevate Your Employee Wellness
+      </h1>
+      <h1 className="text-6xl pt-10 text-center text-white font-bold italic">with</h1>
+      <h1 className="text-6xl text-center pt-10 text-white font-bold">
+        Restory Healthytech's AI-Driven Health Check!
+      </h1>
+      <h3 className="text-white text-2xl pt-10 text-center">
+        Give your employees the most advanced and AI driven annual health
+        checkup
+      </h3>
+      <div className="text-center pt-10">
+ 
+        <button className=" border-solid text-center border-2 my-5 border-l-yellow-50 w-max px-2 py-2 text-white rounded-md">
+          Learn More
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BodySectionOne
+export default BodySectionOne;

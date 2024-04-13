fe6712f43242cb404e45615e2527d9bbfd9aa5b0
Amar pratap <amarprataphzb@gmail.com>
2024-04-13 17:05:15 +0530

    code push of body3

--- src/comonents/herosection/BodySectionThree.jsx
+++ src/comonents/herosection/BodySectionThree.jsx
@@ -5,21 +5,22 @@ import uparrow from '../../assets/uparrow.png'
 
 const BodySectionThree = () => {
   return (
-    <div className=' flex flex-row justify-around pt-16' >
+    <div  style={{backgroundColor:"#6f42c1" ,height:"90vh" }}  className=' flex flex-row justify-around py-32' >
       <div className='w-1/4'>
-           <img  className="m-auto" src={clockicon} alt='image load' height={120} width={120}/> 
-          <h5 className="text-center font-bold my-8  text-2xl">Convenience & Efficiency</h5>
-          <div><p className='text-justify'>This is done at the convenience of their house or workplace based on employee requirement and organizational policy. The report comes as a overall score which is measurable and repeatable enabling comprehensive tracking of their health as a whole and for individual body systems</p></div>
+           <img  className="m-auto rounded-full shadow-md" src={clockicon} alt='image load' height={120} width={120}/> 
+          <h5 className="text-center  shadow-gray-500 font-bold my-8 text-white  text-2xl">Convenience & Efficiency</h5>
+          <div><p className='text-justify te
+          xt-white'>This is done at the convenience of their house or workplace based on employee requirement and organizational policy. The report comes as a overall score which is measurable and repeatable enabling comprehensive tracking of their health as a whole and for individual body systems</p></div>
       </div>
       <div className='w-1/4'>
-          <div> <img  className="m-auto" src={bargraph}  height={120} width={120} />  </div>
-          <h5 className="text-center font-bold my-8  text-2xl">Proactive Healthcare</h5>
-          <div><p className='text-justify'>Early identification of medical, social and nutritional challenges and risks such as cardiac health, bone health, Gut health, brain health, kidney and liver health are provided</p></div>
+          <div> <img  className="m-auto rounded-full shadow-md" src={bargraph}  height={120} width={120} />  </div>
+          <h5 className="text-center shadow-gray-500 font-bold my-8 text-white text-2xl">Proactive Healthcare</h5>
+          <div><p className='text-justify text-white'>Early identification of medical, social and nutritional challenges and risks such as cardiac health, bone health, Gut health, brain health, kidney and liver health are provided</p></div>
       </div>
       <div className='w-1/4'>
-          <div> <img className="m-auto" src={uparrow}  height={120} width={120}/>  </div>
-          <h5 className="text-center font-bold my-8  text-2xl">Increased Productivity</h5>
-          <div><p className='text-justify'>Early identification of issues impacting quality of life, wellness and nutrition and trackable and measurable health insights will improve their productivity and satisfaction</p></div>
+          <div> <img className="m-auto rounded-full shadow-md" src={uparrow}  height={120} width={120}/>  </div>
+          <h5 className="text-center shadow-gray-500 font-bold my-8 text-white text-2xl">Increased Productivity</h5>
+          <div><p className='text-justify text-white'>Early identification of issues impacting quality of life, wellness and nutrition and trackable and measurable health insights will improve their productivity and satisfaction</p></div>
       </div>
     </div>
   )
--- src/comonents/herosection/BodySectionTwo.jsx
+++ src/comonents/herosection/BodySectionTwo.jsx
@@ -1,98 +1,104 @@
-import React, { useState } from 'react'
-import './bodysectiontwo.css'
-import image1 from "../../assets/t-image1.png"
+import React, { useState } from "react";
+import "./bodysectiontwo.css";
+import image1 from "../../assets/t-image1.png";
 import image2 from "../../assets/t-image2.jpg";
 import image3 from "../../assets/t-image3.jpg";
-import leftArrow from '../../assets/leftArrow.png'
-import rightArrow from '../../assets/rightarrow.jpg'
-import {motion} from 'framer-motion'
-
+import leftArrow from "../../assets/leftArrow.png";
+import rightArrow from "../../assets/rightarrow.jpg";
+import { motion } from "framer-motion";
 
 const testimonialsData = [
   {
     image: image1,
     review:
       "What sets Restory apart from others is our holistic approach to health assessment. In addition to standard blood parameters, we consider over 150 lifestyle factors including nutrition, psychological well-being, and social aspects to provide a thorough evaluation of individual health. ",
-    
   },
   {
     image: image2,
-    review: 'Our proprietary AI driven LyfTrac Report synthesizes this information into actionable insights, empowering individuals to make informed and proactive decisions about their health. This is then reviewed by our Medical Team and personalised solutions are provided for each individual tailored to their requirements  ',
-  
+    review:
+      "Our proprietary AI driven LyfTrac Report synthesizes this information into actionable insights, empowering individuals to make informed and proactive decisions about their health. This is then reviewed by our Medical Team and personalised solutions are provided for each individual tailored to their requirements  ",
   },
   {
-    image : image3,
-    review:'Furthermore, Restory conducts prescription audits for all participants, identifying potential drug-drug interactions and instances of medication duplication. Our aim is to ensure that individual receives personalized attention and guidance for optimal health outcomes',
-    
-  }
+    image: image3,
+    review:
+      "Furthermore, Restory conducts prescription audits for all participants, identifying potential drug-drug interactions and instances of medication duplication. Our aim is to ensure that individual receives personalized attention and guidance for optimal health outcomes",
+  },
 ];
 const BodySectionTwo = () => {
-  const transition = {type:"spring", duration:3}
-  const [selected, setSelected] = useState(0)
-  const tLength = testimonialsData.length
- 
-   
-    return (
-      
-   <>
-  <div className='testimonials'>
-  <div className="  text-4xl mt-10 text-center overflow-hidden text-white font-bold">
-   <span> Revolutionize Employee </span>
-            <span className='stroke-text'> with Wellness AI-Powered </span>
-            <span>  Health Assessments</span>
-            </div>
-  </div>
-     <div className='testimonials' id='testimonial'>
-   
-   <div className='left-t mt-20 leading-10 text-xl' style={{lineHeight:"50px"}}>
-       <motion.span
-       key={selected}
-        initial={{opacity: 0, x: -100}}
-        animate={{opacity: 1, x: 0}}
-        exit={{opacity:0, x: 100}}
-        transition={transition}
-       >
-           {testimonialsData[selected].review}
-       </motion.span>
-      
-   </div>
-   <div className='right-t mt-20 overflow-hidden'>
-   <motion.div
-   initial={{opacity:0, x:-100}} 
-   transition={{...transition, duration:2}}
-   whileInView={{opacity:1,x:0}}
-   ></motion.div>
-   <motion.div
-   initial={{opacity:0, x:100}} 
-   transition={{...transition, duration:2}}
-   whileInView={{opacity:1, x:0}}></motion.div>
-       <motion.img
-        key={selected}
-        initial={{opacity: 0, x: 100}}
-        animate={{opacity: 1, x: 0}}
-        exit={{opacity:0, x: -100}}
-        transition={transition}
-       src={testimonialsData[selected].image} alt='' />
-        <div className='arrows'>
-   <img 
-     onClick={() => {
-       selected === 0 ? setSelected(tLength - 1) : setSelected((prev) => prev - 1);
-     }} 
-     src={leftArrow} 
-     alt='' 
-   />
-   <img
-     onClick={() => {
-       selected === tLength - 1 ? setSelected(0) : setSelected((prev) => prev + 1);
-     }} 
-     src={rightArrow} 
-     alt=''  // Make sure alt text is provided
-   />
- </div>
-    </div>
-</div>
-   </>
-  )
-}
+  const transition = { type: "spring", duration: 3 };
+  const [selected, setSelected] = useState(0);
+  const tLength = testimonialsData.length;
 
-export default BodySectionTwo
+  return (
+    <div>
+      <div className="testimonials">
+        <div className="  text-4xl mt-10 text-center overflow-hidden text-white font-bold">
+          <span> Revolutionize Employee </span>
+          <span className="stroke-text"> with Wellness AI-Powered </span>
+          <span> Health Assessments</span>
+        </div>
+      </div>
+      <div className="testimonials" id="testimonial">
+        <div
+          className="left-t my-24 leading-10 text-xl"
+          style={{ lineHeight: "50px" }}
+        >
+          <motion.span
+            key={selected}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 100 }}
+            transition={transition}
+          >
+            {testimonialsData[selected].review}
+          </motion.span>
+        </div>
+        <div className="right-t my-24 overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ ...transition, duration: 2 }}
+            whileInView={{ opacity: 1, x: 0 }}
+          ></motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            transition={{ ...transition, duration: 2 }}
+            whileInView={{ opacity: 1, x: 0 }}
+          ></motion.div>
+          <motion.img
+            key={selected}
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={transition}
+            src={testimonialsData[selected].image}
+            alt=""
+            height={1000}
+            width={600}
+          />
+          <div className="arrows">
+            <img
+              onClick={() => {
+                selected === 0
+                  ? setSelected(tLength - 1)
+                  : setSelected((prev) => prev - 1);
+              }}
+              src={leftArrow}
+              alt=""
+            />
+            <img
+              onClick={() => {
+                selected === tLength - 1
+                  ? setSelected(0)
+                  : setSelected((prev) => prev + 1);
+              }}
+              src={rightArrow}
+              alt="" // Make sure alt text is provided
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BodySectionTwo;

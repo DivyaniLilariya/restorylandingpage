cd04cd7e5956653390b23daf0b584d84debdd2cf
Amar pratap <amarprataphzb@gmail.com>
2024-04-25 13:55:15 +0530

    bodysectionthree

--- src/comonents/herosection/BodySectionOne.jsx
+++ src/comonents/herosection/BodySectionOne.jsx
@@ -7,51 +7,53 @@ const BodySectionOne = () => {
   const transition={type:"spring",duration:3}
   return (
     <div
-      className="z-50 h-dvh sm:w-full mt-24  maindiv "
+      className="z-50 sm:w-full  md:w-32 lg:w-48 mt-24 maindiv"
       style={{
         backgroundImage: `url(${image})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        marginTop:'80px',
-        opacity:0.9,
-        overflow:"hidden"
+        backgroundPosition: "center",
+        opacity: 0.9,
+        overflow: "hidden",
+        height: "100vh", // Set height to 100% of viewport height
+        width: "100%", // Set width to 100% of viewport width
       }}
     >
-      
       <motion.h1
-      initial={{opacity:0,x:-500}}
-      animate={{opacity:1,x:0}}
-      exit={{opacity:0,x:500}}
-      transition={transition}
-      className="text-6xl pt-36 text-center overflow-hidden  text-white font-bold text-resize">
-      Elevate Your Employee Wellness
+        initial={{ opacity: 0, x: -500 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 500 }}
+        transition={transition}
+        className="text-6xl pt-36 text-center overflow-hidden text-white font-bold text-resize"
+      >
+        Elevate Your Employee Wellness
       </motion.h1>
 
       <motion.h1
-      initial={{opacity:0,y:-500}}
-      animate={{opacity:1,y:0}}
-      exit={{opacity:0,y:500}}
-      transition={transition}
-      className="text-6xl pt-10 text-center overflow-hidden italic  text-white font-bold text-resize">
-      With
+        initial={{ opacity: 0, y: -500 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 500 }}
+        transition={transition}
+        className="text-6xl pt-10 text-center overflow-hidden italic text-white font-bold text-resize"
+      >
+        With
       </motion.h1>
 
       <motion.h1
-      initial={{opacity:0,x:-500}}
-      animate={{opacity:1,x:0}}
-      exit={{opacity:0,x:500}}
-      transition={transition}
-      className="text-6xl pt-10 text-center overflow-hidden  text-white font-bold text-resize">
-      Restory Healthytech's AI-Driven Health Check!
+        initial={{ opacity: 0, x: -500 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 500 }}
+        transition={transition}
+        className="text-6xl pt-10 text-center overflow-hidden text-white font-bold text-resize"
+      >
+        Restory Healthytech's AI-Driven Health Check!
       </motion.h1>
 
-     
       <h3 className="text-white text-2xl pt-10 text-center text-resize">
         Give your employees the most advanced and AI driven annual health
         checkup
       </h3>
       <div className="text-center pt-10">
- 
         <button className=" border-solid text-center border-2 my-5 border-l-yellow-50 w-max px-2 py-2 text-white rounded-md">
           Learn More
         </button>
@@ -61,3 +63,5 @@ const BodySectionOne = () => {
 };
 
 export default BodySectionOne;
+
+

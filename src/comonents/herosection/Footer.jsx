1afff0678afd50e3f451e6e9c7c331d58f9933e7
Amar pratap <amarprataphzb@gmail.com>
2024-05-16 09:32:03 +0530

    changes in footer part

--- src/comonents/herosection/Footer.jsx
+++ src/comonents/herosection/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
   };
   return (
     <div>
-      <footer className=" text-center text-neutral-600 bg-blue-900 dark:text-neutral-200 lg:text-left">
+      <footer className=" text-center text-white bg-blue-900 dark:text-neutral-200 lg:text-left">
         {/* social media section */}
         <div className="flex items-center  justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
           <div className="mr-12 hidden lg:block">
@@ -20,7 +20,7 @@ const Footer = () => {
           </div>
           {/* <!-- Social network icons container --> */}
           <div className="flex justify-center">
-            <a href="https://www.facebook.com/restoryhealthytech/" className="mr-6 text-neutral-600  hover:bg-red-500 hover:p-1 hover:rounded-lg dark:text-neutral-200">
+            <a href="https://www.facebook.com/restoryhealthytech/" className="mr-6 text-white  hover:bg-red-500 hover:p-1 hover:rounded-lg dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -30,7 +30,7 @@ const Footer = () => {
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
               </svg>
             </a>
-            <a  href="https://twitter.com/RestoryH"  className="mr-6 text-neutral-600  hover:bg-red-500 hover:p-1 hover:rounded-lg dark:text-neutral-200">
+            <a  href="https://twitter.com/RestoryH"  className="mr-6 text-white  hover:bg-red-500 hover:p-1 hover:rounded-lg dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -41,7 +41,7 @@ const Footer = () => {
               </svg>
             </a>
           
-            <a href="https://www.instagram.com/" className="mr-6 text-neutral-600  hover:bg-red-500 hover:p-1 hover:rounded-lg dark:text-neutral-200">
+            <a href="https://www.instagram.com/" className="mr-6 text-white  hover:bg-red-500 hover:p-1 hover:rounded-lg dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -51,7 +51,7 @@ const Footer = () => {
                 <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
               </svg>
             </a>
-            <a  href="https://www.linkedin.com/company/restory-healthytech/mycompany/" className="mr-6 hover:bg-red-500 hover:p-1 hover:rounded-lg text-neutral-600 dark:text-neutral-200">
+            <a  href="https://www.linkedin.com/company/restory-healthytech/mycompany/" className="mr-6 hover:bg-red-500 hover:p-1 hover:rounded-lg text-white dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -90,20 +90,20 @@ const Footer = () => {
                 links
               </h6>
               <p className="mb-4">
-                <a onClick={()=>{navigate("/"); scrollToTop();}} className="text-neutral-600 dark:text-neutral-200 hover:text-yellow-300">
+                <a onClick={()=>{navigate("/"); scrollToTop();}} className="text-white dark:text-neutral-200 hover:text-yellow-300">
                   Home
                 </a>
               </p>
               <p className="mb-4">
-                <a onClick={()=>{navigate("/about"); scrollToTop();}} className="text-neutral-600 dark:text-neutral-200 hover:text-yellow-300">
+                <a onClick={()=>{navigate("/about"); scrollToTop();}} className="text-white dark:text-neutral-200 hover:text-yellow-300">
                   About Us
                 </a>
               </p>
               <p className="mb-4">
-                <a onClick={()=>{navigate("/services"); scrollToTop();}} className="text-neutral-600 dark:text-neutral-200 hover:text-yellow-300">Services</a>
+                <a onClick={()=>{navigate("/services"); scrollToTop();}} className="text-white dark:text-neutral-200 hover:text-yellow-300">Services</a>
               </p>
               <p>
-                <a  onClick={()=>{navigate("/contact"); scrollToTop();}} className="text-neutral-600 dark:text-neutral-200 hover:text-yellow-300">Contacts</a>
+                <a  onClick={()=>{navigate("/contact"); scrollToTop();}} className="text-white dark:text-neutral-200 hover:text-yellow-300">Contacts</a>
               </p>
             </div>
             {/* <!-- visitus section --> */}
@@ -159,7 +159,7 @@ const Footer = () => {
         <div className="  p-4 text-center bg-black bg-opacity-20">
           <span>© 2024 Copyright : </span>
           <a
-            className="font-semibold text-neutral-600 dark:text-neutral-400"
+            className="font-semibold text-white dark:text-neutral-400"
             href="https://tw-elements.com/"
           >
              ALL RIGHTS RESERVED | DESIGNED AND DEVELOPED BY RESTORY HEALTHYTECH
